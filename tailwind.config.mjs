e0eeea9aefb88b35ca301ff86b82a37213d8edbf
Leo Voon <voonlihhaur@gmail.com>
2024-02-06 13:23:47 +0800

    add fonts, hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,5 @@
+import colors from "tailwindcss/colors";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -5,8 +7,31 @@ export default {
     colors: {
       primary: "red",
       secondary: "blue",
+      gray: colors.gray,
+      white: colors.white,
+      black: colors.black,
     },
-    extend: {},
+    extend: {
+      width: {
+        17: "4.25rem",
+        18: "4.5rem",
+        19: "4.75rem",
+        20: "5rem",
+      },
+      height: {
+        17: "4.25rem",
+        18: "4.5rem",
+        19: "4.75rem",
+        20: "5rem",
+      },
+      fontFamily: {
+        sans: ["Open Sans Variable", "sans-serif"],
+        itali: ["Italianno", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-pattern": 'url("/hero-bg.png")',
+      },
+    },
   },
   plugins: [],
 };
